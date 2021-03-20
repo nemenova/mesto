739eeebc0c1f5689b45593bd1c6fc7f1bc8ae932
Nemenova V <vikalangor@gmail.com>
2021-03-20 21:34:56 +0300

    html n fonts ok

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 let popup = document.querySelector('.popup');
-let openPopupBtn = document.querySelector('.open-popup-btn');
+let openPopupBtn = document.querySelector('.profile__edit-btn');
 let closePopupBtn = document.querySelector('.popup__close-btn');
 let nameInput = document.querySelector('.form__item_el_name');
 let jobInput = document.querySelector('.form__item_el_about');
