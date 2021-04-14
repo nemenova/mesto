cb92b80b6164945130f6ea9fc5b1dd4968bc4f2f
Nemenova V <vikalangor@gmail.com>
2021-04-15 00:44:24 +0300

    validation works

--- scripts/index.js
+++ scripts/index.js
@@ -84,15 +84,36 @@ const closeEditPopupBtn = document.querySelector('.popup__close-btn');
 const closePlacePopupBtn = document.querySelector('.place-btn');
 const closePhotoBtn = document.querySelector('.close-photo-btn');
 const addBtn = document.querySelector('.profile__add-btn');
-
+// const popupOpened = document.querySelector('.popup_opened')
 // функции открытия и закрытия универсальные
 function openPopup(element) {
     element.classList.add('popup_opened');
+    document.addEventListener('keydown', function(evt){
+        if (evt.key === 'Escape') {
+            closePopup(element)
+            // console.log(evt.target.parentNode)
+        };
+    }); 
+    // document.addEventListener('click', closeByOverlay);
 };
 
 function closePopup(element) {
     element.classList.remove('popup_opened');
 };
+
+//  function closeByOverlay(evt){
+//      if (!evt.target.classList.contains('.popup__container')){
+//        closePopup(popupOpened)
+//    }
+    
+//  }
+
+
+
+
+
+
+
 
 // слушатели на кнопки открытия и закрытия поп-апов
 openEditPopupBtn.addEventListener('click', function () {
@@ -145,39 +166,114 @@ formSubmitter.addEventListener('submit', handleEditFormSubmit);
 
 
 
-
 // Вынесем все необходимые элементы формы в константы
 const formElement = document.querySelector('.form');
-const formInput = formElement.querySelector('.form__item');
+// const formInput = formElement.querySelector('.form__item');
+// const formError = formElement.querySelector(`.${formInput.id}-error`);
 
+
+// formInput.addEventListener('input', function (evt) {
+
+//     console.log(evt.target.validity.valid);
+// });
 // Функция, которая добавляет класс с ошибкой
-const showInputError = (element) => {
-    element.classList.add('form__item_type_error');
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add('form__item_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('form__item-error_active');
 };
 
 // Функция, которая удаляет класс с ошибкой
-const hideInputError = (element) => {
-    element.classList.remove('form__item_type_error');
+const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('form__item_type_error');
+    errorElement.classList.remove('form__item-error_active');
+    errorElement.textContent = '';
 };
 
-// Функция isValid теперь принимает formElement и inputElement,
-// а не берёт их из внешней области видимости
-
+// Функция, которая проверяет валидность поля
 const isValid = (formElement, inputElement) => {
     if (!inputElement.validity.valid) {
-        // showInputError теперь получает параметром форму, в которой
-        // находится проверяемое поле, и само это поле
+        // Если поле не проходит валидацию, покажем ошибку
         showInputError(formElement, inputElement, inputElement.validationMessage);
     } else {
-        // hideInputError теперь получает параметром форму, в которой
-        // находится проверяемое поле, и само это поле
+        // Если проходит, скроем
         hideInputError(formElement, inputElement);
     }
 };
-formElement.addEventListener('submit', function (evt) {
-    // Отменим стандартное поведение по сабмиту
-    evt.preventDefault();
-});
 
-// Вызовем функцию isValid на каждый ввод символа
-formInput.addEventListener('input', isValid);
+// formElement.addEventListener('submit', function (evt) {
+//     // Отменим стандартное поведение по сабмиту
+//     evt.preventDefault();
+// });
+
+// // Вызовем функцию isValid на каждый ввод символа
+// formInput.addEventListener('input', isValid); 
+
+const toggleButtonState = (inputList, buttonElement) => {
+    // Если есть хотя бы один невалидный инпут
+    if (hasInvalidInput(inputList)) {
+        // сделай кнопку неактивной
+        buttonElement.classList.add('popup__submit-btn_inactive');
+        buttonElement.setAttribute("disabled", "disabled");
+    } else {
+        // иначе сделай кнопку активной
+        buttonElement.classList.remove('popup__submit-btn_inactive');
+        buttonElement.removeAttribute("disabled", "disabled");
+    }
+};
+
+const setEventListeners = (formElement) => {
+    // Находим все поля внутри формы,
+    // сделаем из них массив методом Array.from
+    const inputList = Array.from(formElement.querySelectorAll('.form__item'));
+    const buttonElement = formElement.querySelector('.popup__submit-btn');
+    toggleButtonState(inputList, buttonElement);
+    // Обойдём все элементы полученной коллекции
+    inputList.forEach((inputElement) => {
+        // каждому полю добавим обработчик события input
+        inputElement.addEventListener('input', () => {
+            // Внутри колбэка вызовем isValid,
+            // передав ей форму и проверяемый элемент
+            isValid(formElement, inputElement)
+            
+        });
+    });
+}; 
+
+const hasInvalidInput = (inputList) => {
+    // проходим по этому массиву методом some
+    return inputList.some((inputElement) => {
+        // Если поле не валидно, колбэк вернёт true
+        // Обход массива прекратится и вся фунцкция
+        // hasInvalidInput вернёт true
+
+        return !inputElement.validity.valid;
+    })
+}; 
+
+
+
+const enableValidation = () => {
+    // Найдём все формы с указанным классом в DOM,
+    // сделаем из них массив методом Array.from
+    const formList = Array.from(document.querySelectorAll('.form'));
+
+    // Переберём полученную коллекцию
+    formList.forEach((formElement) => {
+        formElement.addEventListener('submit', (evt) => {
+            // У каждой формы отменим стандартное поведение
+            evt.preventDefault();
+        });
+
+        // Для каждой формы вызовем функцию setEventListeners,
+        // передав ей элемент формы
+        setEventListeners(formElement);
+    });
+};
+
+// Вызовем функцию
+enableValidation(); 
+
+
